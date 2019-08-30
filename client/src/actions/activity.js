ac6094d77c349ea1447c950dad806396ac03db9e
Guillermo <gpilleux46@gmail.com>
2019-08-30 10:00:36 -0400

    types

---
+++ client/src/actions/activity.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import {
+  GET_ACTIVITIES,
+  GET_ACTIVITY,
+  ADD_ACTIVITY,
+  UPDATE_ACTIVITY,
+  DELETE_ACTIVITY
+} from './types';
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -31,3 +31,26 @@ export const ADD_POST = 'ADD_POST';
 
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+// Tipos App Movilidad
+// Actividades
+export const GET_ACTIVITIES = 'GET_ACTIVITIES';
+export const GET_ACTIVITY = 'GET_ACTIVITY';
+export const ADD_ACTIVITY = 'ADD_ACTIVITY';
+export const UPDATE_ACTIVITY = 'UPDATE_ACTIVITY';
+export const DELETE_ACTIVITY = 'DELETE_ACTIVITY';
+export const ACTIVITY_ERROR = 'ACTIVITY_ERROR';
+
+// Visitantes
+export const GET_VISITORS = 'GET_VISITORS';
+export const GET_VISITOR = 'GET_VISITOR';
+export const ADD_VISITOR = 'ADD_VISITOR';
+export const UPDATE_VISITOR = 'UPDATE_VISITOR';
+export const DELETE_VISITOR = 'DELETE_VISITOR';
+export const VISITOR_ERROR = 'VISITOR_ERROR';
+
+// Visita
+export const ADD_VISITOR_TO_VISIT = 'ADD_VISITOR_TO_VISIT';
+export const DELETE_VISITOR_IN_VISIT = 'DELETE_VISITOR_IN_VISIT';
+export const GET_VISITORS_IN_VISIT = 'GET_VISITORS_IN_VISIT';
+export const VISIT_ERROR = 'VISIT_ERROR';
